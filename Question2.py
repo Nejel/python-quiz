9aab6a81ffd4d705dca28ed387c4c28de48fe73f
Nejel <nejel@mail.ru>
2017-11-08 16:40:23 +0300

    2
    2

--- Question2.py
+++ Question2.py
@@ -7,8 +7,17 @@ for num in ns:
 
 print(odds)
 
-
+'''
 A. SyntaxError
 B. [1, 3, 5, 7, 9]
 C. [02, 04, 06, 08, 10]
 D. ValueError
+'''
+
+
+'''
+File "<ipython-input-2-9257b2e996c9>", line 1
+    ns = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10]
+           ^
+SyntaxError: invalid token
+'''
